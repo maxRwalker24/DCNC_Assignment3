95a390363e271481c6e11fd86940add516a2f1a8
maxwalker <maxwalkerguitar@gmail.com>
2025-11-10 13:22:28 +1100

    change server ip

--- app/chatbot_ui.py
+++ app/chatbot_ui.py
@@ -131,5 +131,6 @@ def build_ui():
 
 if __name__ == "__main__":
     ui = build_ui()
-    ui.launch(server_name="0.0.0.0", server_port=7860)
+    ui.launch(server_name="127.0.0.1", server_port=7860)
+
 
